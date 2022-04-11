6600f9af541117de149d2d6a80d76cab47320230
Chrisann0509 <chrisannlee97@gmail.com>
2022-04-11 09:23:51 +0800

    get swap latest layout

--- src/components/SwapComponent/SwapComponent.js
+++ src/components/SwapComponent/SwapComponent.js
@@ -6,94 +6,101 @@ import ethLogo from '../../images/eth.jpeg';
 import Header from '../Header/Header.js';
 import '../SwapComponent/SwapComponent.css';
 import DragonSwap from '../../abis/DragonSwap.json'
+import Web3 from 'web3';
 
 export default function SwapComponent() {
 
-        //load the metamask account and display on web page
-        async function loadBlockchainData() {
-            const web3 = window.web3
-            //load account
-            const accounts = await web3.eth.getAccounts()
-            console.log(accounts)
-            // this.setState({ account: accounts[0] })  //error
-    
-           // console.log(DragonSwap.abi, DragonSwap.networks[5777].address)
-            const networkId = await web3.eth.net.getId()
-           
-            const networkData = DragonSwap.networks[networkId]
-            if (networkData) {
-                const dragonswap = new web3.eth.Contract(DragonSwap.abi, networkData.address)
-                // this.setState({ marketplace })
-                console.log('Marketplace' + dragonswap)
-                const tokenCount = await dragonswap.methods.token1().call()
-                console.log("product:" + tokenCount.toString())
-    
-            } else {
-                window.alert('Marketplace contract not deployed to detected network')
-    
-            }
-    
-            //const abi = Marketplace.abi
-            //const address = Marketplace.networks[5777].address
-            //make it dynamic to get network ID
-            //const address = Marketplace.networks[networkId].address
-            //const marketplace = new web3.eth.Contract(abi, address)
-            //console.log(marketplace)
+    //load the metamask account and display on web page
+    async function loadBlockchainData() {
+        const web3 = new Web3(window.web3.currentProvider);
+        //load account
+        const accounts = await web3.eth.requestAccounts()
+
+        // this.setState({ account: accounts[0] })  //error
+
+        // console.log(DragonSwap.abi, DragonSwap.networks[5777].address)
+        const networkId = await web3.eth.net.getId()
+
+        const networkData = DragonSwap.networks[networkId]
+        if (networkData) {
+            const dragonswap = new web3.eth.Contract(DragonSwap.abi, networkData.address)
+
+            console.log('Marketplace' + dragonswap)
+            const tokenCount = await dragonswap.methods.token1().call()
+            console.log("product:" + tokenCount.toString())
+
+        } else {
+            window.alert('Marketplace contract not deployed to detected network')
+
         }
+    }
+
     return (
-        <div style={{height: "100vh",background: "#F8F8F8" }}>
+        <div style={{ height: "100vh", background: "#F8F8F8" }}>
             <Header />
-            <div className="wrapper">
-                <div className="content">
-                    <div className="formHeader">
-                        <div>Swap</div>
-                        <div>
-                            <RiSettings3Fill />
+            <form onSubmit={(event) => {
+          event.preventDefault()
+          const name = this.token1.value
+          const price = window.web3.utils.toWei(this.token1.value.toString(), 'Ether')
+          this.props.createProduct(name, price)
+
+        }}>
+                <div className="wrapper">
+
+                    <div className="content">
+                        <div className="formHeader">
+                            <div>Swap</div>
+                            <div>
+                                <RiSettings3Fill />
+                            </div>
                         </div>
-                    </div>
-                    <div className="transferPropContainer">
-                        <input
-                            type='text'
-                            className="transferPropInput"
-                            placeholder='0.0'
-                            pattern='^[0-9]*[.,]?[0-9]*$'
+                        <div className="transferPropContainer">
+                            <input
+                                type='number'
+                                className="transferPropInput"
+                                placeholder='0.0'
+                                pattern='^[0-9]*[.,]?[0-9]*$'
+                                id="productPrice"
+                               // ref={(input) => { this.token1 = input }}
                             // onChange={e => handleChange(e, 'amount')}
-                        />
-                        <div className="currencySelector">
-                            <div className="currencySelectorContent">
-                                <div className="currencySelectorIcon">
-                                    <img className="ethLogo" src={ethLogo} alt='eth logo' />
+                            />
+                            <div className="currencySelector">
+                                <div className="currencySelectorContent">
+                                    <div className="currencySelectorIcon">
+                                        <img className="ethLogo" src={ethLogo} alt='eth logo' />
+                                    </div>
+                                    <div className="currencySelectorTicker">ETH</div>
+                                    <AiOutlineDown className="currencySelectorArrow" />
                                 </div>
-                                <div className="currencySelectorTicker">ETH</div>
-                                <AiOutlineDown className="currencySelectorArrow" />
                             </div>
                         </div>
-                    </div>
-                    <div className="transferPropContainer">
-                        <input
-                            type='text'
-                            className="transferPropInput"
-                            placeholder='0x...'
+                        <div className="transferPropContainer">
+                            <input
+                                type='text'
+                                className="transferPropInput"
+                                placeholder='0x...'
                             // onChange={e => handleChange(e, 'addressTo')}
-                        />
-                        <div className="currencySelector">
-                            <div className="currencySelectorContent">
-                                <div className="currencySelectorTicker">Select a Token</div>
-                                <AiOutlineDown className="currencySelectorArrow" />
+                            />
+                            <div className="currencySelector">
+                                <div className="currencySelectorContent">
+                                    <div className="currencySelectorTicker">Select a Token</div>
+                                    <AiOutlineDown className="currencySelectorArrow" />
+                                </div>
                             </div>
                         </div>
+                        <div className="confirmButton">
+                            Confirm
+                        </div>
+                        {/* <div onClick={e => handleSubmit(e)} className={style.confirmButton}>
+                     Confirm
+                 </div> */}
                     </div>
-                    <div className="confirmButton">
-                        Confirm
-                    </div>
-                    {/* <div onClick={e => handleSubmit(e)} className={style.confirmButton}>
-                        Confirm
-                    </div> */}
+                    {/* <Modal isOpen={!!router.query.loading} style={customStyles}>
+                 <TransactionLoader />
+             </Modal> */}
                 </div>
-                {/* <Modal isOpen={!!router.query.loading} style={customStyles}>
-                    <TransactionLoader />
-                </Modal> */}
-            </div>
+            </form>
+
         </div>
     );
 }
